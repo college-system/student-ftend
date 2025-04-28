126b5a272ebfdf079d74ae2161c886c429b80413
Benease <kelvinbernald4@gmail.com>
2025-04-28 12:35:04 +0100

    push from main laptop

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar =() => {
+    return (
+        <div className="flex items-center justify-between p-4">
+            {/* search bar */}
+            <div className="">
+                <Image/>
+            </div>
+            {/* icons and user*/}
+            <div className=""></div>
+        </div>
+    )
+}
+
+export default Navbar
